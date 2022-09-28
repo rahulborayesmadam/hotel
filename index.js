11e71a7c08411e8d28c95c71712889cff670f211
= <=>
2022-09-28 18:45:57 +0530

    REST For booking created

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+app.use(cors());
+const port = process.env.PORT || 7080;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+// middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.listen(port, () => {
+  connect();
+  console.log(`Connected to Backend ${port} number`);
+});
